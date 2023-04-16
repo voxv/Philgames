4fed0c1c3ab40640ba190623e91cd05c055860eb
blaboazo <blop11ca@yahoo.ca>
2023-04-16 14:14:49 -0400

    test

--- games/includes.js
+++ games/includes.js
@@ -18,8 +18,13 @@ function ismobile() {
 var metaTag=document.createElement('meta');
 metaTag.name = "viewport"
 var scale = 0.67
-if (ismobile())
-	scale = 0.4;
+if (ismobile()) {
+	if (GAME_NAME=='ulcer') {
+		scale = 0.5;
+	} else {
+		scale = 0.4;
+	}
+}
 
 metaTag.content = "width=device-width, initial-scale="+scale+", maximum-scale="+scale+", user-scalable=0"
 
