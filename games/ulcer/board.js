bda5034bed3f23782641c3c11d1180c2992ef490
blaboazo <blop11ca@yahoo.ca>
2023-04-16 14:51:14 -0400

    test

--- games/ulcer/board.js
+++ games/ulcer/board.js
@@ -48,7 +48,7 @@ var gridopacity = 1;
 		{
 			var c = this.get('container');
 
-			c.append('<img style="position:absolute; left:180px; top:0px; width:940px; height:940px;"  src="./images/board.png"/>');
+			c.append('<img style="position:absolute; left:280px; top:0px; width:940px; height:940px;"  src="./images/board.png"/>');
 			/*var popper = this.popperview = new Y.PopperView();
 			popper.get('container').setStyle('position','absolute');
 			popper.get('container').setStyle('left',popperposition[0]);
